74d6582c6e44907c1f39374bf1b416669a88c897
shelldubai26 <shelldubai26@gmail.com>
2026-08-31 21:07:05 +0000

    Switch public showroom to premium V2

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import PublicShowroom from './components/PublicShowroom'
+import PublicShowroomV2 from './components/PublicShowroomV2'
 import './styles.css'
 import './extras.css'
 import './table-fixes.css'
 import './sidebar-scroll.css'
 import './showroom.css'
 const publicRoute=window.location.pathname.startsWith('/showroom')
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>{publicRoute?<PublicShowroom/>:<App/>}</React.StrictMode>)
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>{publicRoute?<PublicShowroomV2/>:<App/>}</React.StrictMode>)
