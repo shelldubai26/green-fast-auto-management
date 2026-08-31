090a4e8d9de015ff069112a3e99c65e1e02bbfaf
shelldubai26 <shelldubai26@gmail.com>
2026-08-31 20:16:58 +0000

    Wire public showroom entry route

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import PublicShowroom from './components/PublicShowroom'
 import './styles.css'
 import './extras.css'
 import './table-fixes.css'
 import './sidebar-scroll.css'
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
+import './showroom.css'
+const publicRoute=window.location.pathname.startsWith('/showroom')
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>{publicRoute?<PublicShowroom/>:<App/>}</React.StrictMode>)
