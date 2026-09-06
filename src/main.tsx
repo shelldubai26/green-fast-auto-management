43819d744c979fa9528e209d64bdef591249b73d
shelldubai26 <shelldubai26@gmail.com>
2026-09-06 00:15:08 +0000

    Route public TikTok tracking gateway

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import PublicShowroomV2 from './components/PublicShowroomV2'
 import PublicLegalPage from './components/PublicLegalPage'
+import PublicTrackingGateway from './components/PublicTrackingGateway'
 import './styles.css'
 import './extras.css'
 import './table-fixes.css'
@@ -10,5 +11,5 @@ import './sidebar-scroll.css'
 import './showroom.css'
 
 const path=window.location.pathname
-const page=path.startsWith('/showroom')?<PublicShowroomV2/>:path==='/terms'?<PublicLegalPage kind="terms"/>:path==='/privacy'?<PublicLegalPage kind="privacy"/>:<App/>
+const page=path.startsWith('/showroom')?<PublicShowroomV2/>:path.startsWith('/t/')?<PublicTrackingGateway/>:path==='/terms'?<PublicLegalPage kind="terms"/>:path==='/privacy'?<PublicLegalPage kind="privacy"/>:<App/>
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>{page}</React.StrictMode>)
