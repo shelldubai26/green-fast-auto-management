04afd719e78caa3589938bc0c17afdcafcfdbd79
shelldubai26 <shelldubai26@gmail.com>
2026-09-15 07:13:31 +0800

    Load marketplace detail, specs and comparison styles

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ import './showroom.css'
 import './ui-polish.css'
 import './components/MarketplaceV1.home.css'
 import './components/MarketplaceV1.intent.css'
+import './components/MarketplaceV1.detail.css'
 
 const path=window.location.pathname
 const isTerms=path==='/terms'||path.startsWith('/terms/')
